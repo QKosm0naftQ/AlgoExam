ddbfdb98f10b92c62ca3870a2dc77228d216bd5a
Костянтин Підодвірний <kostya190807@gmail.com>
2025-05-18 22:03:02 +0200

    Готові класи

---
+++ Algorithm/Quick_sort.cs
@@ -0,0 +1,13 @@
+﻿using AlgoExam.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoExam.Algorithm
+{
+    internal class Quick_sort : ISortService
+    {
+    }
+}
